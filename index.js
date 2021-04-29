bca748c92d146a742e73399225958304bae5e40e
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-04-28 20:40:14 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin (array) {
+    const win = array.find(obj => {return obj.result === 'W'})
+    if (win != undefined) {
+        return win.year
+    } else {
+        return undefined
+    }
+}
